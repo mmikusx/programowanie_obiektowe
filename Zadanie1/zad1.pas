caad3494dd55c680ea4f2bbdb0937b4bff889b78
mmikusx <73282940+mmikusx@users.noreply.github.com>
2024-03-14 16:16:15 +0100

    Update zad1.pas
    Version 3 (4.0)

--- Zadanie1/zad1.pas
+++ Zadanie1/zad1.pas
@@ -1,20 +1,26 @@
 program GenerowanieOrazSortowanieTablicy;
 
 const
-  N = 50;
+  MAX_N = 200;
 
-procedure GenerujOrazSortujLiczby;
+procedure GenerujOrazSortujLiczby(od, do_: integer; ile: integer);
 var
-  liczby: array[1..N] of integer;
+  liczby: array[1..MAX_N] of integer;
   i, j, temp: integer;
 begin
+  if ile > MAX_N then
+  begin
+    writeln(Przekroczono maksymalną liczbę generowanych liczb.');
+    Exit;
+  end;
+  
   Randomize;
 
-  for i := 1 to N do
-    liczby[i] := Random(101);
+  for i := 1 to ile do
+    liczby[i] := Random(do_ - od + 1) + od;
 
-  for i := 1 to N - 1 do
-    for j := 1 to N - i do
+  for i := 1 to ile - 1 do
+    for j := 1 to ile - i do
       if liczby[j] > liczby[j + 1] then
       begin
         temp := liczby[j];
@@ -24,11 +30,11 @@ begin
 
   writeln('Wygenerowane i posortowane liczby rosnąco:');
   write('[ ');
-  for i := 1 to N - 1 do
+  for i := 1 to ile - 1 do
     write(liczby[i], ', ');
-  writeln(liczby[N], ' ]');
+  writeln(liczby[ile], ' ]');
 end;
 
 begin
-  GenerujOrazSortujLiczby;
+  GenerujOrazSortujLiczby(1, 100, 20);
 end.
